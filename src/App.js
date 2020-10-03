c3f68f37793c1d11662eda9e891af0e4afdbb114
NaimChaabene <naim.chaabene@gmail.com>
2020-10-03 16:16:43 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import myWonderfulImage from "./myImage2.png"
+
+
+
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  let htmlElement1=( <h1 className="title ">Naim Chaabene</h1>); 
+  let htmlElement2=(<img className= "img" src="../images/myImage1.jpg"   alt ='myImage' />); //image dans dossier public
+  let htmlElement3=(<img src={myWonderfulImage}  className= "img" alt ='myImage' />); //image dans dossier src
+  let Firstglobalelement=(<div style={{border:"solid 1px black",maxWidth:"600px", textAlign: "center"}}>
+                           {htmlElement1}  <br></br> {htmlElement2} <br></br>  {htmlElement3} </div>);
+  let SecondElement=(
+                    <video className= "vid" width="320" height="240" controls>
+                     <source src="../videos/video1.mp4" type="video/mp4" />
+                    </video>
+                 );
+
+  return (<div> {Firstglobalelement}    {SecondElement}</div> );
 }
 
 export default App;
